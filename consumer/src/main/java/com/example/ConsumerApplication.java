abf144a8f5f7b337dec4ad00e4322d9ddecf4e00
Pierre Filliolaud <pierre.filliolaud@gmail.com>
2016-11-15 23:35:13 +0100

    Add Spring Integration Sample projects

---
+++ consumer/src/main/java/com/example/ConsumerApplication.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.messaging.SubscribableChannel;
+
+interface ConsumerChannels {
+	
+	@Input
+	SubscribableChannel producer();
+}
+
+
+@SpringBootApplication
+@EnableBinding(ConsumerChannels.class)
+public class ConsumerApplication {
+	
+	@Bean
+	@Scope("prototype")
+	Logger logger(InjectionPoint ip) {
+		return Logger.getLogger(ip.getDeclaredType().getName());
+	}
+	
+	@Bean
+	IntegrationFlow integrationFlow (Logger logger, ConsumerChannels channel) {
+		return IntegrationFlows
+				.from(channel.producer())
+				.handle( String.class, (payload, header)->{
+					logger.info( "new message:" + payload);
+					return null;
+				})
+				.get();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+}
+
+
---
+++ producer/README.md
@@ -0,0 +1,9 @@
+# Video
+https://youtu.be/HQ00E60kB6c
+
+# Run rabbitMQ
+sbin/rabbitmq-server
+
+
+# Execute in command line
+curl -d{} http://localhost:8000/greet/World
